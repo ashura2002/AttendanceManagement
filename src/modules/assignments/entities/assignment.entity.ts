c1a60148427862aff216798795b8d108183b0934
Jmark Dayna <jemarcdayna352@gmail.com>
2025-11-13 20:42:27 +0800

    added table relation between assignment and room

--- src/modules/assignments/entities/assignment.entity.ts
+++ src/modules/assignments/entities/assignment.entity.ts
@@ -1,5 +1,6 @@
 import { Remarks } from 'src/common/enums/remarkOptions.enum';
 import { ScheduleSubject } from 'src/common/enums/scheduleSubject.enum';
+import { Room } from 'src/modules/rooms/entities/room.entity';
 import { Subject } from 'src/modules/subjects/entities/subject.entity';
 import { User } from 'src/modules/users/entities/user.entity';
 import {
@@ -34,4 +35,7 @@ export class AssignmentSubject {
   @ManyToMany(() => Subject, (subject) => subject.assignments)
   @JoinTable()
   subjects: Subject[];
+
+  @ManyToOne(() => Room, (room) => room.assign)
+  room: Room;
 }
