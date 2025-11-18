011579419a8332a9fb183dc53b406503b9c702b8
Jmark Dayna <jemarcdayna352@gmail.com>
2025-11-18 10:45:55 +0800

    added time in and time out on controller and service

--- src/modules/attendances/attendances.controller.ts
+++ src/modules/attendances/attendances.controller.ts
@@ -1,20 +1,36 @@
-import { Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RoleGuard } from 'src/common/guards/role.guard';
 import { AttendancesService } from './attendances.service';
+import { Attendance } from './entities/attendance.entity';
+import { SkipThrottle } from '@nestjs/throttler';
 
 @Controller('attendances')
 @ApiBearerAuth('access-token')
+@SkipThrottle() // --- TEMPORARY RANI 
 @UseGuards(JwtAuthGuard, RoleGuard)
 export class AttendancesController {
-    constructor(private readonly attendanceService: AttendancesService
-    ) { }
+  constructor(private readonly attendanceService: AttendancesService) {}
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    async timeIn(@Req() req): Promise<any> {
-        const { userId } = req.user
-        return await this.attendanceService.timeIn(userId)
-    }
+  @Post('time-in')
+  @HttpCode(HttpStatus.CREATED)
+  async timeIn(@Req() req): Promise<Attendance> {
+    const { userId } = req.user;
+    return await this.attendanceService.timeIn(userId);
+  }
+
+  @Post('time-out')
+  @HttpCode(HttpStatus.CREATED)
+  async timeOut(@Req() req): Promise<Attendance> {
+    const { userId } = req.user;
+    return await this.attendanceService.timeOut(userId)
+  }
 }
--- src/modules/attendances/entities/attendance.entity.ts
+++ src/modules/attendances/entities/attendance.entity.ts
@@ -1,32 +1,34 @@
-import { Remarks } from "src/common/enums/remarkOptions.enum";
-import { SubjectAssignment } from "src/modules/subjectAssignment/entities/subjectAssignment.entity";
-import { User } from "src/modules/users/entities/user.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Remarks } from 'src/common/enums/remarkOptions.enum';
+import { SubjectAssignment } from 'src/modules/subjectAssignment/entities/subjectAssignment.entity';
+import { User } from 'src/modules/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Attendance {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type: 'date' })
-    date: string;
+  @Column({ type: 'date' })
+  date: string;
 
-    @Column({ type: 'time', nullable: true })
-    timeIn: string;
+  @Column({ type: 'time', nullable: true })
+  timeIn: string;
 
-    @Column({ type: 'time', nullable: true })
-    timeOut: string;
+  @Column({ type: 'time', nullable: true })
+  timeOut: string;
 
-    @Column({ type: 'float', default: 0 })
-    totalHours: number;
+  @Column({ type: 'float', default: 0 })
+  totalHours: number;
 
-    @Column({ type: 'enum', enum: Remarks, default: Remarks.NoClockInRecord })
-    remarks: Remarks;
+  @Column({ type: 'enum', enum: Remarks, default: Remarks.NoClockInRecord })
+  attendanceStatus: Remarks;
 
-    @ManyToOne(() => SubjectAssignment, (assign) => assign.attendance)
-    assignment: SubjectAssignment;
+  @Column({ type: 'enum', enum: Remarks, default: Remarks.Schedule })
+  remarks: Remarks;
 
-    @ManyToOne(() => User, (user) => user.attendance)
-    user: User;
+  @ManyToOne(() => SubjectAssignment, (assign) => assign.attendance)
+  assignment: SubjectAssignment;
+
+  @ManyToOne(() => User, (user) => user.attendance)
+  user: User;
 }
-    
